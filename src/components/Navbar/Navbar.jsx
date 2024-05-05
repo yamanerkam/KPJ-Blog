822e3ec287b01cf2545fba996ba6d4fcfbf84ba4
Kam <erkamyaman35@gmail.com>
2024-05-05 14:55:40 +0300

    deploy-issue2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,6 @@ import './Navbar.css'
 
 import { Link } from 'react-router-dom';
 import { LuSunMedium } from "react-icons/lu";
-import { CiDark } from "react-icons/ci";
 
 
 export default function Navbar() {
