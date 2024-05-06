dda430b42729016e10c809783691194f7adb17a7
Kam <erkamyaman35@gmail.com>
2024-05-06 20:50:17 +0300

    dark-mode

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import './Navbar.css'
 
 import { Link } from 'react-router-dom';
 import { LuSunMedium } from "react-icons/lu";
 import { CiDark } from "react-icons/ci";
-
+import { ThemeContext } from '../../Contexts/ThemeContext/ThemeContext';
 
 export default function Navbar() {
-    const [darkMode, setDarkMode] = useState(false)
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode)
-    }
+    const { theme, toggleTheme } = useContext(ThemeContext);
+    useEffect(() => {
+        document.body.classList.toggle('light-theme', theme === 'light');
+        document.body.classList.toggle('dark-theme', theme === 'dark');
+    }, [theme]);
     return (
         <div className='navbar'>
+
             <div className='main-part'>
                 <h1>KPJ</h1>
                 <div className="pages">
@@ -20,6 +22,7 @@ export default function Navbar() {
                     <a>
                         <Link to={'/'}>Home</Link>
                     </a>
+
 
                     <a>
                         <Link to={'/blog'}>Blog</Link>
@@ -37,18 +40,13 @@ export default function Navbar() {
             </div>
 
 
-            {darkMode ? (
-                <button onClick={toggleDarkMode} className='btn'>
-                    <LuSunMedium size={25} className='icon'></LuSunMedium>
-
-                </button>
-            )
-                : (
-                    <button onClick={toggleDarkMode} className='btn'>
-                        <CiDark size={25} className='icon'></CiDark>
-
-                    </button>
+            <button onClick={toggleTheme} className='btn'>
+                {theme === 'light' ? (
+                    <CiDark color='#3b7aa2' size={25} className='icon'></CiDark>
+                ) : (
+                    <LuSunMedium color='white' size={25} className='icon'></LuSunMedium>
                 )}
+            </button>
 
 
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom"
-
+import { ThemeProvider } from './Contexts/ThemeContext/ThemeContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+
   </React.StrictMode>,
 )
